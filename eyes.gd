50680d9380372a137b5607cd4dba188f74a5f7ea
gaw7 <mrgaw07@gmail.com>
2025-01-03 01:00:02 -0500

    Update eyes.gd (Camera Rotation)
    This update is mainly for allowing the camera to rotate around the main node ("eyes", normally) via mouse motion. HOWEVER, It should be noted that using Camera Mode "Always Forward" REQUIRES a parent node for Eyes.

--- eyes.gd
+++ eyes.gd
@@ -11,6 +11,10 @@ var son = null
 func _ready() -> void:
 	_createSpring()
 	_createCam()
+	
+	#for the mouse
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 
 func _createSpring():
@@ -21,7 +25,7 @@ func _createSpring():
 	s.spring_length = 5
 	
 	#transforms
-	s.rotation.x = deg_to_rad(-30.0)
+	s.rotation.x = deg_to_rad(0.0)
 	s.position += targetPos
 	
 	#apply
@@ -31,3 +35,40 @@ func _createSpring():
 func _createCam():
 	var c = Camera3D.new()
 	son.add_child(c)
+
+
+#for rotating the camera...
+##How sensitive should the mouse be, in x,y fashion?
+@export var camSens = Vector2(0.1, 0.1)
+##how far up can you look? Down?
+@export var camLimit = 50.0
+var camLevel = 0
+
+enum CamType {AlwaysForward, RotateAround}
+##Does the character always have back to camera?
+##Does the character only rotate when moving?
+@export var CameraMode: CamType 
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		
+		var RotateAroundY = deg_to_rad(-event.relative.x * camSens.x)
+		var RotateAroundX = -event.relative.y * camSens.y
+		
+		if CameraMode == CamType.AlwaysForward:
+			_rotY(self.get_parent(), RotateAroundY)
+			_rotX(self, RotateAroundX)
+		elif CameraMode == CamType.RotateAround:
+			_rotY(self, RotateAroundY)
+			_rotX(son, RotateAroundX)
+
+
+func _rotY(axis, rad):
+	axis.rotate_y(rad)
+
+func _rotX(axis, dY):
+	#var dY = -event.relative.y * camSens.y
+	if camLevel+dY >-camLimit && camLevel+dY < camLimit:
+		camLevel+=dY
+		axis.rotate_x(deg_to_rad(dY))
